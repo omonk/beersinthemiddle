7dd01eee034d8f36ad313909ffbab0fb6f7106fc
omonk <ollie@olliemonk.com>
2017-09-09 21:36:09 +0100

    add run server function

--- server/services/location-queries.service.js
+++ server/services/location-queries.service.js
@@ -1,7 +1,4 @@
 module.exports = (req, res) => {
-    console.log(req.query);
     const queries = req.query; 
-    console.log("------------------------");
-    console.log("location hit");
     console.log(queries);
 };
